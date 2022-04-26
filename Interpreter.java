d0de951bb22edc5b2c09b4f89157597e0357158c
José Fonte <73178551+josefonte@users.noreply.github.com>
2022-04-26 17:55:32 +0100

    Update Interpreter.java

--- Interpreter.java
+++ Interpreter.java
@@ -1,22 +1,15 @@
-//lê o ficheiro de config
-/*Metodos
-* - lista de casas
-* - lista de devices por casa
-* - lista de deviçoes por casa
-* - lista de devices por divisão
-*
-* - criar hashmap dos speakers
-* -
-*
-*
-*/
-import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.io.FileNotFoundException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
+/**
+ * O interpreter recebe um ficheiro config e cria a configuração inicial apartir do mesmo.
+ * Esta configuração inicial significa que é criada um hashmap com as keys sendo os nomes da casa associados às suas casa inteligentes
+ *
+ * @author josefonte
+ * @version 0.1
+ */
 
 public class Interpreter {
     File configfile;
@@ -45,14 +38,15 @@ public class Interpreter {
         if (this==o) return true;
         if (this.getClass()!=o.getClass()) return false;
         Interpreter file = (Interpreter) o;
-
-        try {
-            return FileUtils.contentEquals(configfile, file);
+        return true;
+        /*try {
+           // return FileUtils.contentEquals(configfile, file);
+            return true;
         } catch (IOException e)
         {
             e.printStackTrace();
             return false;
-        }
+        }*/
     }
 
     public String toString(){
@@ -73,9 +67,161 @@ public class Interpreter {
         return new Interpreter(this);
     }
 
-    /*public List<CasaInteligente> lista_casas(){
+
+
+
+    public HashMap<String, CasaInteligente> creator(){
+
+        HashMap<String,CasaInteligente> config = new HashMap<>();
+
+        try {
+            Scanner sc = new Scanner(this.configfile);
+            String file_line;
+            String[] parser;
+            while (sc.hasNextLine()){
+                file_line= sc.nextLine();
+                parser= file_line.split(";");
+                if(parser[0].equals("houses-config")) config.put(parser[1], createhouse(parser[1]));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred. File not located or not open correctly");
+        }
+
+        return config;
+    }
+
+
+
+
+    public CasaInteligente createhouse(String housename){
+
+        CasaInteligente casa = new CasaInteligente();
+
+        try {
+            Scanner sc = new Scanner(this.configfile);
+            String file_line;
+            String[] parser;
+
+            while (sc.hasNextLine()){
+                file_line= sc.nextLine();
+                parser= file_line.split(";");
+
+                if(parser[0].equals("houses-config") && parser[1].equals(housename)){
+                    casa.setOwner(parser[2]);
+                    casa.setNif(parser[3]);
+                    String[] parser2 = parser[4].split("-");
+                    for (int i=0;i<parser2.length;i++) casa.addRoom(parser2[i]);
+                }
+
+                if(parser[0].equals(housename)){
+                    String room = parser[1];
+                    String[] parser2 = parser[2].split("-");
+                    SmartDevice smt = new SmartDevice();
+
+                    switch (parser2[0]) {
+                        case "smtblb" -> {
+                            boolean state = Boolean.parseBoolean(parser[3]);
+                            float custo = Float.parseFloat(parser[4]);
+                            int tone = Integer.parseInt(parser[5]);
+                            int dimensao = Integer.parseInt(parser[6]);
+                            float valor_fixo = Float.parseFloat(parser[7]);
+
+                            smt = new SmartBulb(parser[2], state, custo, tone, dimensao, valor_fixo);
+
+                            break;
+                        }
+                        case "smtspk" -> {
+                            boolean state = Boolean.parseBoolean(parser[3]);
+                            float custo = Float.parseFloat(parser[4]);
+                            int volume = Integer.parseInt(parser[5]);
+
+                            smt = new SmartSpeaker(parser[2], state, custo, volume, parser[6], parser[7]);
+                            break;
+                        }
+                        case "smtcmr" -> {
+                            boolean state = Boolean.parseBoolean(parser[3]);
+                            float custo = Float.parseFloat(parser[4]);
+                            int resolution = Integer.parseInt(parser[5]);
+                            int file_size = Integer.parseInt(parser[6]);
+
+                            smt = new SmartCamera(parser[2], state, custo, resolution, file_size);
+                            break;
+                        }
+                    }
+
+                    casa.addDevice(smt);
+
+                    if(casa.hasRoom(parser[1])) casa.addDeviceToRoom(room, smt.getID());
+                    else {
+                        List<String> devs_ids = new ArrayList<>();
+                        devs_ids.add(smt.getID());
+                        casa.addRoom(room,devs_ids);
+                    }
+
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred. File not located or not open correctly");
+        }
+
+        return casa;
+    }
+
+
+    public HashMap<String, Float> speakerConfig(){
+        HashMap<String,Float> speaker_config = new HashMap<>();
+
+        try {
+            Scanner sc = new Scanner(this.configfile);
+            String file_line;
+            String[] parser;
+            while (sc.hasNextLine()){
+                file_line= sc.nextLine();
+                parser= file_line.split(";");
+                if(parser[0].equals("speaker-brands-config")) speaker_config.put(parser[1], Float.parseFloat(parser[2]));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred. File not located or not open correctly");
+        }
+
+        return speaker_config;
+    }
+
+/*
+    public HashMap<String, String> lista_casasEdivisoes(){
+        HashMap<String,String> houses_config = new HashMap<>();
+
+        try {
+            Scanner sc = new Scanner(this.configfile);
+            String file_line;
+            String[] parser;
+            while (sc.hasNextLine()){
+                file_line= sc.nextLine();
+                parser= file_line.split(";");
+                if(parser[0].equals("houses-config")) houses_config.put(parser[1], Float.parseFloat(parser[2]));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred. File not located or not open correctly");
+        }
+
+        return houses_config;
 
     }
+
+
+    public List<String> getHouses(){
+        List<String> houses =  new ArrayList<>();
+
+
+
+        return houses;
+
+    }
+
+     public CasaInteligente createhouse(String housename){
+
+
+     }
 
     public List<SmartDevice> lista_dvs(String Casa){
 
@@ -85,5 +231,14 @@ public class Interpreter {
 
     }*/
 
+    public Set<String> casas(){
+        return creator().keySet();
+    }
+
+    public CasaInteligente getCasaInteligente(String housename){
+        return creator().get(housename);
+    }
+
+    
 
 }
