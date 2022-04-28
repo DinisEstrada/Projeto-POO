a93a94331b017b8111b7d7947df9134f0ab13d31
José Fonte <73178551+josefonte@users.noreply.github.com>
2022-04-29 00:45:26 +0100

    Update FornecedorA.java

--- FornecedorA.java
+++ FornecedorA.java
@@ -13,20 +13,17 @@ public class FornecedorA extends Fornecedor{
     }
 
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("\n### FornecedorA ###")
-                .append("\nFornecedor: ").append(super.getName())
-                .append("\nValorBase: ").append(super.getValor_base())
-                .append("\nImposto: ").append(super.getImposto())
-                .append("\nDesconto: ").append(super.getDesconto())
-                .append("\n");
-
-        return sb.toString();
+        return  "\n### FornecedorA ###" +
+                "\nFornecedor: " + super.getName() +
+                "\nValorBase: " + super.getValor_base() +
+                "\nImposto: " + super.getImposto() +
+                "\nDesconto: " + super.getDesconto() +
+                "\n";
     }
 
     public FornecedorA clone(){return new FornecedorA(this);}
 
-    public float precodiapordispositivo(SmartDevice smt, CasaInteligente house){
+    public float formulaPreco(SmartDevice smt, CasaInteligente house){
         return (super.getValor_base()*smt.getConsumo()* (1+super.getImposto()/100))*(1-super.getDesconto()/100);
     }
 }
