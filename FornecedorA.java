4083a0c86eb5e7f01754212dee5ea321c5c04b4b
José Fonte <73178551+josefonte@users.noreply.github.com>
2022-05-02 11:42:54 +0100

    Add files via upload

--- FornecedorA.java
+++ FornecedorA.java
@@ -38,8 +38,8 @@ public class FornecedorA extends Fornecedor{
             throw new SmartDeviceException("SmartDevice não existe/Valores Inválidos");
         else if (super.getDesconto() < 0 || super.getImposto() < 0 || super.getValor_base() < 0)
             throw new FornecedorException("Valores Negativos");
-        else if (casa == null || casa.existsDevice(smt.getID()))
-            throw new CasaInteligenteException("CasaNULL/SmartDevice não existe na casa " + smt.getID());
+        else if (casa==null || !casa.getDevices().containsKey(smt.getID())){
+            throw new CasaInteligenteException("CasaNULL/SmartDevice não existe na casa " + smt.getID());}
         else {
             return (super.getValor_base()*smt.getConsumo()* (1+super.getImposto()/100))*(1-super.getDesconto()/100);
         }
--- FornecedorC.java
+++ FornecedorC.java
@@ -38,8 +38,8 @@ public class FornecedorC extends Fornecedor{
             throw new SmartDeviceException("SmartDevice não existe/Valores Inválidos");
         else if (super.getDesconto() < 0 || super.getImposto() < 0 || super.getValor_base() < 0)
             throw new FornecedorException("Valores Negativos");
-        else if (casa == null || casa.existsDevice(smt.getID()))
-            throw new CasaInteligenteException("CasaNULL/SmartDevice não existe na casa "+ smt.getID());
+        else if (casa==null || !casa.getDevices().containsKey(smt.getID())){
+            throw new CasaInteligenteException("CasaNULL/SmartDevice não existe na casa " + smt.getID());}
         else {
             if (casa.numberDevices() < 10)
                 return (super.getValor_base() * smt.getConsumo() * (1 + super.getImposto() / 100)) * (1 - (super.getDesconto()) / 100);
